440aab674a871e8bb8745d53c2372a4f769fef67
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-09-28 15:01:07 +0200

    Add workaround for Webpack issue
    https://github.com/webpack/webpack/issues/17711

--- typescript-npm/next.config.js
+++ typescript-npm/next.config.js
@@ -4,5 +4,10 @@ module.exports = {
   env: {
     theoplayerLicenseString: 'your_license_string',
     theoplayerLibraryLocation: '/vendor/theoplayer/'
+  },
+  webpack: (config, _options) => {
+    // Workaround for https://github.com/webpack/webpack/issues/17711
+    config.optimization.innerGraph = false;
+    return config;
   }
 }
