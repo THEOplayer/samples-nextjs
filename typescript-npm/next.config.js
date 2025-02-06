354e8a3a5b235db858be414aeebd4095741b5c9b
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-02-06 15:42:42 +0100

    Remove obsolete workaround

--- typescript-npm/next.config.js
+++ typescript-npm/next.config.js
@@ -4,10 +4,5 @@ module.exports = {
   env: {
     theoplayerLicenseString: 'your_license_string',
     theoplayerLibraryLocation: '/vendor/theoplayer/'
-  },
-  webpack: (config, _options) => {
-    // Workaround for https://github.com/webpack/webpack/issues/17711
-    config.optimization.innerGraph = false;
-    return config;
   }
 }
